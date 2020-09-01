6e3593f11bfb6fdc6af0b41291c10c95b9ffb754
saizhuolin <saizhuolin@gmail.com>
2020-09-01 18:56:54 +0800

    delete unuseful import

--- thumbing/shared/src/main/java/com/thumbing/shared/entity/mongo/MongoEditionEntity.java
+++ thumbing/shared/src/main/java/com/thumbing/shared/entity/mongo/MongoEditionEntity.java
@@ -1,11 +1,9 @@
 package com.thumbing.shared.entity.mongo;
 
-import com.thumbing.shared.constants.EntityConstants;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldNameConstants;
 
-import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
